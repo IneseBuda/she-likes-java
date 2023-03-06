982af7a8e0b6e051a3fbc1f74322e2d1402f0073
Ineseb <inesebuda@inbox.lv>
2023-03-06 21:33:10 +0200

    Calculator - sum implemented

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("calculator...");
+       System.out.println("Calculation sum of two numbers");
+        System.out.println("Provide first number:");
+       int firstNumber;
+       int secondNumber;
+       //TODO explain object creation
+       Scanner inputReader = new Scanner(System.in);
+       firstNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + firstNumber);
+        System.out.println("Provide second number:");
+        secondNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + secondNumber);
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum is: " + sum);
+
 
     }
 
