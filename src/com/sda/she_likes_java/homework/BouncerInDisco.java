1483388d183dafc252f8fe93de815cf81780903d
Ineseb <inesebuda@inbox.lv>
2023-03-10 14:56:55 +0200

    not correct if age is less but higt is ok

--- src/com/sda/she_likes_java/homework/BouncerInDisco.java
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -28,19 +28,17 @@ public class BouncerInDisco {
         int personHight = inputReader.nextInt();
         boolean isShort = hight < 170;
         System.out.println("is short: " + isShort);
+        if (personAge < 16) if (personHight < 170) {
+                System.out.println("You do not meet the criteria for attending a disco!");
+            } else {
+                System.out.println("Welcome to disco");
 
-        if (personAge >= 16)
-            if (personHight >= 170) {
+            }
+        if (personAge >= 16) if (personHight >= 170) {
                 System.out.println("Welcome to disco");
             } else {
                 System.out.println("You do not meet the criteria for attending a disco!");
-                if (personAge < 16)
-                    if (personHight < 170) {
-                        System.out.println("Welcome to disco");
-                    } else {
-                        System.out.println("You do not meet the criteria for attending a disco!");
 
-                    }
             }
     }
 }
