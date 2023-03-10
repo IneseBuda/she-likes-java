a4d8c8c1d8ee0ab1c72d103a0c8597352009c1ff
Ineseb <inesebuda@inbox.lv>
2023-03-10 15:37:39 +0200

    without false true, only one wrong age 12 and hight 190, its alowe to go to disco

--- src/com/sda/she_likes_java/homework/BouncerInDisco.java
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -11,35 +11,31 @@ import java.util.Scanner;
 public class BouncerInDisco {
     public static void main(String[] args) {
         System.out.println("Homework BouncerInDisco:");
-        boolean condition = false;
-        if (condition) {
-            System.out.println("This will be run only when condition is false");
-        } else {
-            System.out.println("This will be run when condition is true");
-        }
+
         int age = 16;
         int hight = 170;
         Scanner inputReader = new Scanner(System.in);
-        System.out.println("Tell me how old are You");
+        System.out.println("Tell me how old are You?");
         int personAge = inputReader.nextInt();
-        boolean isKid = age < 16;
-        System.out.println("is kid:" + isKid);
+
         System.out.println("Tell me how tall are You in cm?");
         int personHight = inputReader.nextInt();
-        boolean isShort = hight < 170;
-        System.out.println("is short: " + isShort);
-        if (personAge < 16) if (personHight >= 170) if (personAge >= 16) if (personHight < 170){
+
+
+    if (personAge < 16) if (personHight < 170) {
             System.out.println("You do not meet the criteria for attending a disco!");
         } else {
             System.out.println("Welcome to disco");
-            if (personAge < 16) if (personHight < 170) if(personAge >= 16) if (personHight >= 170) {
-                System.out.println("You do not meet the criteria for attending a disco!");
-            } else {
-                System.out.println("Welcome to disco");
+
+        }
+        if (personAge >= 16) if (personHight >= 170) {
+            System.out.println("Welcome to disco");
+        } else {
+            System.out.println("You do not meet the criteria for attending a disco!");
 
             }
         }
-    }}
+    }
 
 
 
