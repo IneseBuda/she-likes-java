69c08329ef9ca5e3d8d6a553ececd585fc18ea6b
Ineseb <inesebuda@inbox.lv>
2023-03-10 10:05:20 +0200

    adding homework no 9

---
+++ src/com/sda/she_likes_java/homwork/BouncerInDisco.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.homwork;
+//Exercise 9.
+//Bouncer in a disco
+//Prepare a new class BouncerInDisco and in it
+//- get the user’s age and height from the console
+//- only people who are at least 16 years old and taller (or shorter than - at your choice) can enter the disco
+//- print a text saying how the verification went in both cases
+public class BouncerInDisco {
+
+}
