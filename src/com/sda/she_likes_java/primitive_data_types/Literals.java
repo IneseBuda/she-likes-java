96127bee85fb3b18d63d32a6e326ea8fa526a582
Ineseb <inesebuda@inbox.lv>
2023-03-10 21:19:23 +0200

    literals explained

---
+++ src/com/sda/she_likes_java/primitive_data_types/Literals.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.primitive_data_types;
+
+public class Literals {
+    public static void main(String[] args) {
+        // literal is direct value
+        //String literal is e.g. "name"
+        String  name = "Inese";
+
+
+        //integer Literal
+        int age = 40; // 40 is of type of int
+
+        //long literal
+        long anotherAge = 30L;
+
+        //double literal
+        double doubleNumber = 3.1;
+
+        //float literal
+        float floatNumber = 3.1F;
+
+    }
+}
