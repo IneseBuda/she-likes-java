283cea899caf47188136bad897ee4e6b2f825355
Ineseb <inesebuda@inbox.lv>
2023-03-20 14:59:04 +0200

    Sort with Arrays method

--- src/com/sda/she_likes_java/homework/RandomSortNumbers.java
+++ src/com/sda/she_likes_java/homework/RandomSortNumbers.java
@@ -2,6 +2,7 @@ package com.sda.she_likes_java.homework;
 
 
 import javax.swing.*;
+import java.util.Arrays;
 import java.util.Random;
 
 
@@ -12,17 +13,19 @@ public class RandomSortNumbers {
     public static void main(String[] args) {
 
 
-        Random random = new Random();
+        Random randomNumber = new Random();
 
-        int number1 = random.nextInt(10);
-        int number2 = random.nextInt(10);
-        int number3 = random.nextInt(10);
+        int number1 = randomNumber.nextInt(10);
+        int number2 = randomNumber.nextInt(10);
+        int number3 = randomNumber.nextInt(10);
 
         System.out.println(number1);
         System.out.println(number2);
         System.out.println(number3);
-        String fromSmallestToLargest = number1 <=0; number2 != number1; number3>=number2;
-        System.out.println("Random numbers from smallest to largest:" + fromSmallestToLargest);
-
+        int[] arr_int = {number1, number2, number3};
+        Arrays.sort(arr_int);
+        System.out.println("Numbers from smallest to largest" + Arrays.toString(arr_int));
     }
 }
+
+
