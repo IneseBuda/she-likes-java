2fbf79fe9fd907402d60452dba7f77e8aff2fcc3
Ineseb <inesebuda@inbox.lv>
2023-03-21 16:07:24 +0200

    Homework for array

---
+++ src/com/sda/she_likes_java/homework/Arrey.java
@@ -0,0 +1,20 @@
+package com.sda.she_likes_java.homework;
+
+public class Arrey {
+    //Exercise 13
+    //Create an int array with 100 elements
+    //Initialise each element with a value 3 times greater than the current index
+    //e.g.
+    //tab[0] = 3 * 0
+    //tab[10] = 3 * 10.
+    public static void main(String[] args) {
+        int[] myFond = new int[100];
+
+        for (int index = 0; index < myFond.length; index++){
+            myFond[index] = index;
+        }
+        for (int index = 0; index < myFond.length; index++){
+            System.out.println("My fond eur:" + index + "is:" + (index * 3));
+        }
+    }
+}
