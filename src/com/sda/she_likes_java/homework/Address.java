7a5a6259847efbe05b09627b863c45a375d5422c
Ineseb <inesebuda@inbox.lv>
2023-03-23 16:44:42 +0200

    Exercise 14. Adresses

---
+++ src/com/sda/she_likes_java/homework/Address.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.homework;
+
+public class Address {
+    String country;
+    String city;
+    String street;
+    int streetNumber;
+    int flatNumber;
+    String regionLetter;
+    int postalCode;
+
+}
