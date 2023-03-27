dd4486a3d64d744aba48fc8a950c4ecae3888fe4
Ineseb <inesebuda@inbox.lv>
2023-03-27 21:39:01 +0300

    how to use getter and setter

--- src/com/sda/she_likes_java/classes/constructor/FullPerson.java
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -1,10 +1,10 @@
 package com.sda.she_likes_java.classes.constructor;
 
 public class FullPerson {
-    String name;
-    String surname;
-    int age;
-    FullAddress address;
+    private String name;
+   private String surname;
+  private int age;
+   private FullAddress address;
 
     public FullPerson() {
     }
@@ -13,6 +13,40 @@ public class FullPerson {
         this.name = name;
         this.surname = surname;
         this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+                return age;
+    }
+
+    public void setAge(int age) {
+        if(age >0) {
+            this.age = age;
+        }
+    }
+
+    public FullAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(FullAddress address) {
         this.address = address;
     }
 
