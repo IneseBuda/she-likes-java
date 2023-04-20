da71d45549476c8b44cafa90e5ed7c54ba2b443c
Ineseb <inesebuda@inbox.lv>
2023-04-20 21:32:25 +0300

    airplane take off and landing without coordinates

--- src/com/sda/she_likes_java/homework/exercise18/Airplane.java
+++ src/com/sda/she_likes_java/homework/exercise18/Airplane.java
@@ -17,42 +17,112 @@ When implementing, remember that:
  */
 public class Airplane {
     private String flyToFaro;
-    private String onTheAir;
-    private String inHangar;
+
+    private int xTakeOff;
+    private int yPlaneLands;
+    private int zOntheAir;
+    private int aInHagar;
     private int xCoordinates;
     private int yCoordinates;
 
-    public Airplane(String flyToFaro, String onTheAir, String inHangar, int xCoordinates, int yCoordinates) {
+    public Airplane(String flyToFaro,  int xTakeOff, int yPlaneLands, int zOntheAir, int aInHnagar, int xCoordinates, int yCoordinates) {
         this.flyToFaro = flyToFaro;
-        this.onTheAir = onTheAir;
-        this.inHangar = inHangar;
+
+        this.xTakeOff = xTakeOff;
+        this.yPlaneLands = yPlaneLands;
+        this.zOntheAir = zOntheAir;
+        this.aInHagar = aInHnagar;
         this.xCoordinates = xCoordinates;
         this.yCoordinates = yCoordinates;
     }
 
+    public void setFlyToFaro(String flyToFaro) {
+        this.flyToFaro = flyToFaro;
+    }
 
+    public void setxTakeOff(int xTakeOff) {
+        this.xTakeOff = xTakeOff;
+    }
+
+    public void setyPlaneLands(int yPlaneLands) {
+        this.yPlaneLands = yPlaneLands;
+    }
+
+    public void setzOntheAir(int zOntheAir) {
+        this.zOntheAir = zOntheAir;
+    }
+
+    public void setaInHagar(int aInHagar) {
+        this.aInHagar = aInHagar;
+    }
+
+    public void setxCoordinates(int xCoordinates) {
+        this.xCoordinates = xCoordinates;
+    }
+
+    public void setyCoordinates(int yCoordinates) {
+        this.yCoordinates = yCoordinates;
+    }
+
+    public String getFlyToFaro() {
+        return flyToFaro;
+    }
+
+
+
+    public int getxTakeOff() {
+        return xTakeOff;
+    }
+
+    public int getyPlaneLands() {
+        return yPlaneLands;
+    }
+
+    public int getzOntheAir() {
+        return zOntheAir;
+    }
+
+    public int getaInHnagar() {
+        return aInHagar;
+    }
+
+    public int getxCoordinates() {
+        return xCoordinates;
+    }
+
+    public int getyCoordinates() {
+        return yCoordinates;
+    }
 
     public void planeTakeOff() {
+
+        System.out.println("Plane take off from airport");
 
     }
 
     public void thePlaneLands() {
-
+        //yPlaneLands--;
+        System.out.println("Plane are landing");
     }
 
     public void thePlaneOnTheAir() {
-
+        System.out.println("Plane in the air to Faro");
     }
-    public void flyToFaro(){
 
+    public void flyToFaro() {
+        xCoordinates = 5;
+        yCoordinates = 5;
     }
 
     @Override
     public String toString() {
         return "Airplane{" +
                 "flyToFaro='" + flyToFaro + '\'' +
-                ", onTheAir='" + onTheAir + '\'' +
-                ", inHangar='" + inHangar + '\'' +
+
+                ", xTakeOff=" + xTakeOff +
+                ", yPlaneLands=" + yPlaneLands +
+                ", zOntheAir=" + zOntheAir +
+                ", aInHnagar=" + aInHagar +
                 ", xCoordinates=" + xCoordinates +
                 ", yCoordinates=" + yCoordinates +
                 '}';
--- src/com/sda/she_likes_java/homework/exercise18/Airport.java
+++ src/com/sda/she_likes_java/homework/exercise18/Airport.java
@@ -1,4 +1,42 @@
 package com.sda.she_likes_java.homework.exercise18;
 
 public class Airport {
+    public static void main(String[] args) {
+        Airplane airplaneToFaro = new Airplane("Riga.Faro", 10, 1, 11, 0, 5, 5);
+
+        System.out.println("My airplane from Riga to Faro" + airplaneToFaro);
+
+
+        while (true) {
+
+
+            int xTakeOff = airplaneToFaro.getxTakeOff();
+            if (xTakeOff < 11) {
+                airplaneToFaro.planeTakeOff();
+
+                int zOnTheAir = airplaneToFaro.getzOntheAir();
+                zOnTheAir = xTakeOff + 1;
+                if (zOnTheAir > 10) {
+
+                    airplaneToFaro.thePlaneOnTheAir();
+                }
+
+                int yPlaneLands = airplaneToFaro.getyPlaneLands();
+                if (yPlaneLands == 1) {
+
+                    airplaneToFaro.thePlaneLands();
+
+                }
+                int aInHagar = airplaneToFaro.getaInHnagar();
+                if (aInHagar < 1) {
+
+                    System.out.println("Plane is in Hangar");
+
+                }
+                break;
+            }
+        }
+
+
+    }
 }
