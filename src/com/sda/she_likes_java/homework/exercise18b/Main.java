e168bec3d214369d507b0945c33c78e36dc269b9
Ineseb <inesebuda@inbox.lv>
2023-04-25 16:01:40 +0300

    Mariusz airplane example complete

--- src/com/sda/she_likes_java/homework/exercise18b/Main.java
+++ src/com/sda/she_likes_java/homework/exercise18b/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Airplane myAirplane = new Airplane();
         myAirplane.takeOff();
-        myAirplane.takeOff();
+       myAirplane.takeOff();
         System.out.println("Let’s go to Warsaw");
         myAirplane.flyTo(21, 52);
         myAirplane.land();
