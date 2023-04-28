71987da4c7427603b2db633e6462133609dbfb39
Ineseb <inesebuda@inbox.lv>
2023-04-28 15:45:10 +0300

    creating main ex21

---
+++ src/com/sda/she_likes_java/homework/exercise21/Main.java
@@ -0,0 +1,20 @@
+package com.sda.she_likes_java.homework.exercise21;
+
+public class Main {
+    public static void main(String[] args) {
+Vehicle vehicle = new Vehicle();
+vehicle.move();
+
+Airplane airplane = new Airplane();
+airplane.move();
+airplane.flying();
+
+Ship ship = new Ship();
+ship.move();
+ship.floating();
+
+Submarine submarine = new Submarine();
+submarine.move();
+submarine.diving();
+    }
+}
