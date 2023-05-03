fef5b84a869fb477aa5acd8dc1d4367b942ce1e7
Ineseb <inesebuda@inbox.lv>
2023-05-03 11:48:31 +0300

    Ex22.classes for products listing

---
+++ src/com/sda/she_likes_java/homework/exercise22/CocaCola.java
@@ -0,0 +1,35 @@
+package com.sda.she_likes_java.homework.exercise22;
+
+public class CocaCola {
+    private String name;
+    private double price;
+
+    public CocaCola(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "CocaCola{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise22/Main.java
@@ -0,0 +1,4 @@
+package com.sda.she_likes_java.homework.exercise22;
+
+public class Main {
+}
