54a6c400a9010530299c2112a339275567f4ffd5
Ineseb <inesebuda@inbox.lv>
2023-05-18 21:33:27 +0300

    Exception throw

---
+++ src/com/sda/she_likes_java/exceptions/BankExample.java
@@ -0,0 +1,28 @@
+package com.sda.she_likes_java.exceptions;
+
+public class BankExample {
+    public static void main(String[] args) {
+        BankConnector connector = new BankConnector();
+        int balance = connector.accountBalanceAlwaysSuccessful();
+        if(balance>0){
+            System.out.println("I have got money");
+        }else {
+            System.out.println("Debit");
+        }
+
+        System.out.println("Let's try to check balance of my account");
+
+        int myMoney = 0;
+        try {
+            myMoney = connector.accountBalance();
+            System.out.println("My money: "+myMoney);
+        } catch (Exception e) {
+            System.out.println("Unexpected condition occure");
+            System.out.println("Exception:" + e);
+        }
+
+
+        System.out.println("Here");
+
+    }
+}
