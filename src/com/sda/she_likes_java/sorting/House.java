92e2a357eb8cc8b0e4766e6a5a850b1d2f959ed2
Ineseb <inesebuda@inbox.lv>
2023-05-20 16:43:10 +0300

    Exercise with comporator House plus rooms

---
+++ src/com/sda/she_likes_java/sorting/House.java
@@ -0,0 +1,64 @@
+package com.sda.she_likes_java.sorting;
+
+import java.util.Objects;
+
+public class House implements Comparable<House> {
+    private int numberOfRooms;
+
+    private int numberOfFloors;
+
+    private int numberOfBathrooms;
+
+    private String address;
+
+    public House(int numberOfRooms, int numberOfFloors, int numberOfBathrooms, String address) {
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfFloors = numberOfFloors;
+        this.numberOfBathrooms = numberOfBathrooms;
+        this.address = address;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public int getNumberOfBathrooms() {
+        return numberOfBathrooms;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "numberOfRooms=" + numberOfRooms +
+                ", numberOfFloors=" + numberOfFloors +
+                ", numberOfBathrooms=" + numberOfBathrooms +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        House house = (House) o;
+        return numberOfRooms == house.numberOfRooms && numberOfFloors == house.numberOfFloors && numberOfBathrooms == house.numberOfBathrooms && Objects.equals(address, house.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numberOfRooms, numberOfFloors, numberOfBathrooms, address);
+    }
+
+    @Override
+    public int compareTo(House o) {
+        return numberOfBathrooms - o.numberOfBathrooms;
+    }
+}
