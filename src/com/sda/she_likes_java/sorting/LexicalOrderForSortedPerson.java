67a38cd302910778ab21f1f798e7449ae6fc55a2
Ineseb <inesebuda@inbox.lv>
2023-05-20 16:01:45 +0300

    comparator for SortedPersons based on name(reversed)

--- src/com/sda/she_likes_java/sorting/LexicalOrderForSortedPerson.java
+++ src/com/sda/she_likes_java/sorting/LexicalOrderForSortedPerson.java
@@ -6,6 +6,10 @@ public class LexicalOrderForSortedPerson implements Comparator<SortedPerson> {
 
     @Override
     public int compare(SortedPerson o1, SortedPerson o2) {
-        return 0;
+        String name1 = o1.getName();//this the same as below
+        String name2 = o2.getName();//this the same as below
+        return name1.compareTo(name2);//this the same as below
+      //  return o1.getName().compareTo(o2.getName());
+      //  return - o1.getName().compareTo(o2.getName());//for revers order
     }
 }
--- src/com/sda/she_likes_java/sorting/SortingPersonsMain.java
+++ src/com/sda/she_likes_java/sorting/SortingPersonsMain.java
@@ -11,25 +11,40 @@ public class SortingPersonsMain {
         SortedPerson olaf = new SortedPerson("Olaf", 22);
         SortedPerson inese = new SortedPerson("Inese", 12);
         SortedPerson zorro = new SortedPerson("Zorro", 12);
+        SortedPerson zorra = new SortedPerson("Zorra", 12);
         List<SortedPerson> persons = new ArrayList<>();
         persons.add(olaf);
         persons.add(inese);
         persons.add(zorro);
         persons.add(zorro);
-        persons.add(zorro);
+        persons.add(zorra);
 
         Collections.sort(persons);
 
-        System.out.println("After sorting "+ persons);
+        System.out.println("After sorting " + persons);
         //if returns -1 that means: olaf should be first, inese
         //if returns 1 that means: inese first, olfs after
-       // System.out.println("Olaf.compareTo(gustav): "+ olaf.compareTo(inese));
+        // System.out.println("Olaf.compareTo(gustav): "+ olaf.compareTo(inese));
         //System.out.println("Inese.compareTo(zorro): "+ inese.compareTo(zorro));
 
-        Comparator<SortedPerson>reverseAgeOrder = new AgedReversedOrderOfSortedPersons();
-        Collections.sort(persons,reverseAgeOrder);
-       // Collections.sort(persons,new AgedReversedOrderOfSortedPersons());//this is the same as above
+        Comparator<SortedPerson> reverseAgeOrder = new AgedReversedOrderOfSortedPersons();
+        Collections.sort(persons, reverseAgeOrder);
+        // Collections.sort(persons,new AgedReversedOrderOfSortedPersons());//this is the same as above
         System.out.println("Reversed order based on age: " + persons);
+
+        List<String> names = new ArrayList<>();//comparable method
+        names.add("Ramune");
+        names.add("Lina");
+        names.add("Alma");
+        names.add("Karolina");
+        Collections.sort(names);
+        System.out.println("Sorted manes: " + names);
+
+        Collections.sort(persons, new LexicalOrderForSortedPerson());
+        System.out.println("Persons sorted by name: " + persons);
+
+        Collections.sort(persons, new ReversedLexicalOrderForSortedPerson());
+        System.out.println("Persons reversed sorted by name: " + persons);
     }
 }
 
