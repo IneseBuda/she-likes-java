8eb00be0deeaa50ff1b69bed595bae15808497f3
Ineseb <inesebuda@inbox.lv>
2023-05-21 17:05:57 +0300

    Homework Ex27MyDate

---
+++ src/com/sda/she_likes_java/homework/exercise27/MainMyDate.java
@@ -0,0 +1,4 @@
+package com.sda.she_likes_java.homework.exercise27;
+
+public class MainMyDate {
+}
---
+++ src/com/sda/she_likes_java/homework/exercise27/MyDate.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.homework.exercise27;
+/*
+Exercise 27
+Create a MyDate class containing three fields:
+- year - of type int
+- month - of type int
+- day - of type int
+Implement the Comparable interface for your class.
+Create a Main class, add four MyDate objects to the list and perform sorting.
+ */
+
+
+public class MyDate implements Comparable<MyDate>{
+    private int day;
+    private int month;
+    private int year;
+
+
+    @Override
+    public int compareTo(MyDate o) {
+        return 0;
+    }
+}
