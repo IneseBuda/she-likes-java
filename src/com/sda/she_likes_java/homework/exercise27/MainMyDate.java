b09f1034071c249925bc8b5afc0ae2e1ae9fe220
Ineseb <inesebuda@inbox.lv>
2023-05-21 20:00:29 +0300

    Homework Ex27MyDate, sorted by years

--- src/com/sda/she_likes_java/homework/exercise27/MainMyDate.java
+++ src/com/sda/she_likes_java/homework/exercise27/MainMyDate.java
@@ -1,4 +1,23 @@
 package com.sda.she_likes_java.homework.exercise27;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainMyDate {
+
+    public static void main(String[] args) {
+        MyDate weddingDate = new MyDate(29,12,2001);
+        MyDate firstChildBirth = new MyDate(19,12,2002);
+        MyDate secondChildBirth = new MyDate(2,11,2007);
+        MyDate firstDateWithHusband = new MyDate(25,8,1995);
+        List<MyDate> myDates = new ArrayList<>();
+        myDates.add(weddingDate);
+        myDates.add(firstChildBirth);
+        myDates.add(secondChildBirth);
+        myDates.add(firstDateWithHusband);
+
+        Collections.sort(myDates);
+        System.out.println("My events sorted by year: " + myDates);
+    }
 }
--- src/com/sda/she_likes_java/homework/exercise27/MyDate.java
+++ src/com/sda/she_likes_java/homework/exercise27/MyDate.java
@@ -10,14 +10,58 @@ Create a Main class, add four MyDate objects to the list and perform sorting.
  */
 
 
+import java.util.Objects;
+
 public class MyDate implements Comparable<MyDate>{
     private int day;
     private int month;
     private int year;
 
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyDate myDate = (MyDate) o;
+        return day == myDate.day && month == myDate.month && year == myDate.year;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(day, month, year);
+    }
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
 
     @Override
     public int compareTo(MyDate o) {
-        return 0;
+        return year - o.getYear();
+
     }
+
+
 }
