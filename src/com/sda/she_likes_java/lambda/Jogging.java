096150ce88d3a3ec96d6646a9fe26231dd5a91c8
Ineseb <inesebuda@inbox.lv>
2023-06-05 20:13:57 +0300

    Lambda - default methods

--- src/com/sda/she_likes_java/lambda/Jogging.java
+++ src/com/sda/she_likes_java/lambda/Jogging.java
@@ -1,8 +1,18 @@
 package com.sda.she_likes_java.lambda;
-//API - Application programming interface
+
+//Classes fom java.lang are imported always
+//import java.lang
+// API - Application programming interface
 //contract - what to do, but not how
+@FunctionalInterface//to protect programmer from accidental refactoring
+
 public interface Jogging {
 
     public abstract void run();
+
+    //simple/default implementation
+   default void walk(){};// can not put more methods, because lambda don't know to witch method to apply
+//default void methods needs to be override in classes not with lambda
+    default void drink(String drink){}
 
 }
