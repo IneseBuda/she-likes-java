e0d83b3bbee816e8ad0953e60dc0c034a3ef4e20
Ineseb <inesebuda@inbox.lv>
2023-06-07 20:52:31 +0300

    user repository - REGISTER NEW USER implemented

--- src/com/sda/she_likes_java/final_project/repository/UserRepository.java
+++ src/com/sda/she_likes_java/final_project/repository/UserRepository.java
@@ -1,32 +1,77 @@
 package com.sda.she_likes_java.final_project.repository;
 
 import com.sda.she_likes_java.final_project.entity.User;
+import com.sda.she_likes_java.final_project.exception.InexperiencedUserException;
 
+import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserRepository {
+    private static long userCounter;
+    //=new HashMap this is the same as we insert constructor, constructor creating empty map
+    private Map<Long, User> users;
 
-    public void registerNewUser(){
-
+    public UserRepository() {
+        this.users = new HashMap<>();
     }
-    public void deleteUser(){
 
+    private static Long nextId() {
+        return ++userCounter;
     }
 
-    public List<Object> listAllUsers(){
+    public void registerNewUser(String name,
+                                String surname,
+                                LocalDate dateOfBirth,
+                                String login,
+                                String password,
+                                LocalDate drivingLicenseIssueDate) throws InexperiencedUserException {
+        // check if user is eligible for registering
+        checkDrivingLicense(drivingLicenseIssueDate);
+
+        // get new id
+        Long id = nextId();
+
+        // register
+        registerUser(name, surname, dateOfBirth, login, password, drivingLicenseIssueDate, id);
+    }
+
+    private void registerUser(String name, String surname, LocalDate dateOfBirth, String login, String password, LocalDate drivingLicenseIssueDate, Long id) {
+        User newUser = new User(id, name, surname, dateOfBirth, login, password, drivingLicenseIssueDate);
+        users.put(id, newUser);
+    }
+
+    private void checkDrivingLicense(LocalDate drivingLicenceIssueDate) throws InexperiencedUserException {
+        // at least 3 years:)
+        final int requiredNumberOfYears = 3;
+        LocalDate currentDate = LocalDate.now();
+//        drivingLicenceIssueDate + 3 year is before now
+        LocalDate boundaryDate = drivingLicenceIssueDate.plusYears(requiredNumberOfYears);
+
+        if (boundaryDate.isAfter(currentDate)) {
+            throw new InexperiencedUserException("Better buy a bicycle!");
+        }
+    }
+
+    public void deleteUser() {
+//TODO
+    }
+//TODO
+    public List<User> listAllUsers() {
         return List.of();
     }
 
-    public void updateUser(User user){
-
+    public void updateUser(User user) {
+//TODO
     }
-    //filtering
-    public List<User>getUserBasedOnCriteria(){
+//TODO
+    public List<User> getUsersBasedOnCriteria() {
         return List.of();
     }
-    //order by age
-    public List<User>getOrderedUsers(Comparator<User>comparator){
+//TODO
+    public List<User> getOrderedUsers(Comparator<User> comparator) {
         return List.of();
     }
 }
