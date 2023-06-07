c2c901e26fd70824f1232c4ca542fde7c7e0548c
Ineseb <inesebuda@inbox.lv>
2023-06-07 19:32:19 +0300

    User class created - with records semantic

---
+++ src/com/sda/she_likes_java/final_project/entity/User.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.final_project.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public record User(String name, String surname, LocalDate dateOfBirth, String login, String password,
+                   LocalDate drivingLicenceIssueDate) {
+
+
+
+
+}
