e4f00908132ad11b87bd34fd1a363ea2a3e86c95
Ineseb <inesebuda@inbox.lv>
2023-06-09 19:38:33 +0300

    CAR RELATED MODELS CREATED

--- src/com/sda/she_likes_java/final_project/entity/Car.java
+++ src/com/sda/she_likes_java/final_project/entity/Car.java
@@ -1,7 +1,18 @@
 package com.sda.she_likes_java.final_project.entity;
 
 
+import java.time.YearMonth;
 
 //TODO: add fields
-public record Car(Category category) {
+public record Car(Long id,
+                  String brand,
+                  String model,
+                  GearboxType gearboxType,//ENUM BECAUSE ONLY TWO
+                  CarBody carBody,
+                  PriceCategory category,
+                  FuelType fuelType,
+                  Long mileage,
+                  YearMonth productionDate) {
+
+
 }
