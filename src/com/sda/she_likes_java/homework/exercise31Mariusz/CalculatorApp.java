115cd64ad89fa3d6b87e547e364a20f3edc11846
Ineseb <inesebuda@inbox.lv>
2023-06-09 18:12:00 +0300

    Exercise31.Mariusz version

---
+++ src/com/sda/she_likes_java/homework/exercise31Mariusz/CalculatorApp.java
@@ -0,0 +1,18 @@
+package com.sda.she_likes_java.homework.exercise31Mariusz;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Calculator calculator = (a, b) -> {
+            int result = a + b;
+
+            System.out.printf("%d + %d = %d%n", a, b, result);
+            return result;
+        } ;
+
+        System.out.println("Let's check our calculator");
+        calculator.add(5, 3);
+        calculator.sub(8, 4);
+        calculator.mult(4, 12);
+        calculator.div(10, 5);
+    }
+}
